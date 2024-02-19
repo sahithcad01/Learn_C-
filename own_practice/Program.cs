0157f88e944d21efd0e9327f9acf86bee1983e45
sahithcad01 <sahithcad01@gmail.com>
2024-02-19 23:04:20 +0530

    Own practice C# file added

---
+++ own_practice/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program{
+static void Main(string [] args){
+    int a=10;
+    int b=5;
+    Console.WriteLine("Addition of a & b :" + (a+b));
+    Console.WriteLine("Hello, World!");
+}
+}
